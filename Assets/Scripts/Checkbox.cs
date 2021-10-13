8739d721e56a530d8c8e42c1a806744116a0b6fe
ionamatell <32112541+Sthaim@users.noreply.github.com>
2021-10-13 15:57:09 +0200

    complet avec ancien code

--- Assets/Scripts/Checkbox.cs
+++ Assets/Scripts/Checkbox.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Checkbox : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+public class CheckBox : MonoBehaviour , IPointerClickHandler, IPointerEnterHandler
 {
-    public string Marque;
-
     public Sprite Cross;
     public Sprite Circle;
-
+    public GameController Game;
+    public int ID;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        GetComponent<Image>().sprite = Cross;
+        Debug.Log("saucisse "+(11+11));
+        if (Game.Turn == GameController.TurnState.PlayerOne && GetComponent<Image>().sprite == null)
+        {
+            GetComponent<Image>().sprite = Cross;
+            Game.changeTurn(ID);
+        }
+        else if (Game.Turn == GameController.TurnState.PlayerTwo && GetComponent<Image>().sprite == null)
+        {
+            GetComponent<Image>().sprite = Circle;
+            Game.changeTurn(ID);
+        }
+
+
+        
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Hover me");
+        //Debug.Log("ouille");
     }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Game = FindObjectOfType<GameController>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,12 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameController : MonoBehaviour
 {
+    public enum TurnState
+    {
+        None = 0,
+        PlayerOne=1,
+        PlayerTwo=2
+    };
+
+    public TurnState Turn;
+    public List<int> playerOneChecks;
+    public List<int> playerTwoChecks;
+
     // Start is called before the first frame update
     void Start()
     {
+        //gameObject.SetActive(false);
+        Turn = TurnState.PlayerOne;
         
+    }
+
+    public void changeTurn(int _id)
+    {
+        if (Turn == TurnState.PlayerOne)
+        {
+            Turn = TurnState.PlayerTwo;
+            playerOneChecks.Add(_id);
+
+        }
+        else if (Turn == TurnState.PlayerTwo)
+        {
+            Turn = TurnState.PlayerOne;
+            playerTwoChecks.Add(_id);
+        }
+        checkDiagonals();
+
+        Debug.Log("Current turn =>" + Turn);
+    }
+
+    void checkDiagonals()
+    {
+        if (playerOneChecks.Contains(4) && ((playerOneChecks.Contains(0) && playerOneChecks.Contains(8))|| (playerOneChecks.Contains(2) && playerOneChecks.Contains(6))))
+        {
+            Debug.Log("PlayerOne Wins");
+        }
+
+        if (playerTwoChecks.Contains(4) && ((playerTwoChecks.Contains(0) && playerTwoChecks.Contains(8)) || (playerTwoChecks.Contains(2) && playerTwoChecks.Contains(6))))
+        {
+            Debug.Log("PlayerTwo Wins");
+        }
     }
 
     // Update is called once per frame
